866b7bef7250776f08ba1c1924551709140d082b
mfbevan <matthewbevan00@gmail.com>
2023-09-28 20:18:35 +1000

    refactor: ♻️ pull hbs template from file

--- modules/templates/markdown/index.ts
+++ modules/templates/markdown/index.ts
@@ -1,52 +1,11 @@
+import fs from "fs";
+
 import Handlebars from "handlebars";
 
 import { Template } from "../../../types";
 
-// const functionTemplate = `
-// ### {{name}}
-// \`\`\`solidity
-// {{signature}}
-// \`\`\`
-// TODO comments
-// **Parameters**
-
-// **Returns**
-// `;
-
-const functionTemplate = `
-### {{name}}
-{{#if natspec.notice}}
-{{{natspec.notice}}}
-{{/if}}
-{{#if natspec.dev}}
-_{{{natspec.dev}}}_
-{{/if}}
-
-{{#if signature}}
-\`\`\`solidity
-{{{signature}}}
-\`\`\`
-{{/if}}
-
-{{#if natspec.params}}
-**Parameters**
-
-| Name | Type | Description |
-| ---- | ---- | ----------- |
-{{#each params}}
-| {{name}} | {{type}} | {{{description}}} |
-{{/each}}
-{{/if}}
-
-{{#if natspec.returns}}
-**Return**
-
-| Name | Type | Description |
-| ---- | ---- | ----------- |
-{{#each returns}}
-| {{#if name}}{{name}}{{else}}[{{@index}}]{{/if}} | {{type}} | {{{description}}} |
-{{/each}}
-{{/if}}`;
+const FUNCTION_TEMPLATE_PATH = process.cwd() + "/modules/templates/markdown/function.hbs";
+const functionTemplate = fs.readFileSync(FUNCTION_TEMPLATE_PATH, "utf8");
 
 export const markdownTemplate: Template = {
   func: Handlebars.compile(functionTemplate),
--- test/document-function.test.ts
+++ test/document-function.test.ts
@@ -94,9 +94,7 @@ describe("documentFunction", () => {
 
   test("should return the function signature", () => {
     const output = documentFunction(functions[0], context);
-    expect(output).toInclude(`\`\`\`solidity
-function pureFunction() public pure returns (bool)
-\`\`\``);
+    expect(output).toInclude("function pureFunction() public pure returns (bool)");
   });
 
   test("should return the function natspec", () => {
@@ -110,13 +108,12 @@ function pureFunction() public pure returns (bool)
   });
 
   test("should return the function return values", () => {
-    const output = documentFunction(functions[9], context);
-    expect(output).toInclude("[0] |  | This is a test return value |");
+    const output = documentFunction(functions[8], context);
+    expect(output).toInclude("[0] | bool | This is a test return value |");
   });
 
   test("should return typed return values", () => {
     const output = documentFunction(functions[9], context);
-    console.log(output);
     expect(output).toInclude("| namedReturn | bool | This is a test return value |");
   });
 });
